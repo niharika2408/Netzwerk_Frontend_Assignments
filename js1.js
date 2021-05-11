86dc4472f6ce07eef24f87dcabef28305717d3f4
Niharika <niharikakanjariya@gmail.com>
2021-05-11 14:46:32 +0530

    portfolio

---
+++ js1.js
@@ -0,0 +1,19 @@
+<html>
+<head>
+    <title>js 1</title>
+</head>
+<body>
+<script>
+    var x=10
+    if(x%3==0){
+        document.write("tic")
+    }
+    else if(x%3==0){
+        document.write("tac")
+    }
+    else if(x%3==0 && x%5==0){
+        document.write("toe")
+    }
+</script>
+</body>
+</html>
